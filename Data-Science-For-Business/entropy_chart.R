8df73e748e0c19476e0a0149cba6ddc4db0c0d88
Larry-Hignight <larry.hignight@gmail.com>
2017-07-26 15:30:12 -0700

    Added warnings to the entropy_chart function when either an empty segment is passed or segment with entropy greater than y.height is passed;  Added a class entropy function.

--- Data-Science-For-Business/entropy_chart.R
+++ Data-Science-For-Business/entropy_chart.R
@@ -1,15 +1,33 @@
-# Based on the 'entropy chart' found in Chapter 3 of Data Science for Business
-# Segments should be a list leaf nodes containing the target variable for each segment
-# This versions computes the 'numeric entropy' using the variance function
-entropy_chart <- function(segments, y.height = 1) {
-  seg_n <- sapply(segments, length)
-  seg_proportion <- seg_n / sum(seg_n)
-  seg_var <- sapply(segments, var)
-  seg_var[seg_n == 0] <- 0
-
-  # Adjust the y.height if necessary 
-  # if (max(seg_var) > y.height) y.height <- round(max(seg_var) + .5, 0)
-
-  barplot(height = seg_var, width = seg_proportion, space = 0, ylim=c(0, y.height))
+# Entropy function for a class of items where x is a vector of elements from a given class
+# The vector x should only contain the target variable from a segmentation of the data
+entropy <- function(x) {
+  n <- length(unique(x))  # The number of classes in the current segment
+  if (n == 1) return(0)   # This is a 'pure' segment
+  tbl <- prop.table(table(x))
+  -1 * sum(sapply(tbl, function(p) p * log(p, n)), na.rm = TRUE)
 }
 
+# Based on the 'entropy chart' found in Chapter 3 of Data Science for Business
+# Segments is a list of leaf nodes containing _ONLY_ the target variable for each segment
+#
+# By default, 'numeric entropy' is computed using a version of the variance function 
+# that returns 0 for segments of length 1
+#
+# Use the entropy function above to compute class entropy
+entropy_chart <- function(segments, entropy_f = function(x) if (length(x) == 1) 0 else var(x), y.height = 1) {
+  seg_len <- sapply(segments, length)
+  if (any(seg_len == 0)) {
+    warning("Empty segments have been removed from the entropy chart function")
+    segments <- segments[seg_len > 0]
+    seg_len <- seg_len[seg_len > 0]
+  }
+  
+  # Compute the entropy and warn/adjust the y.height if necessary
+  entropy_measure <- sapply(segments, entropy_f)
+  if (max(entropy_measure) > y.height) {
+    warning("Entropy measured exceeds the y.height value;  Adjusting y.height upwards")
+    y.height <- round(max(entropy_measure) + .5, 0)
+  }
+
+  barplot(height = entropy_measure, width = seg_len / sum(seg_len), space = 0, ylim=c(0, y.height))
+}
